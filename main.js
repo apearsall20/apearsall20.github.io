d7b63f61f812d80c3473d0ab4d85cc371fe63e2a
apearsall20 <apearsall20@gmail.com>
2021-07-25 15:30:42 -0500

    more data

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 async function init() {
-	data = await d3.csv("https://raw.githubusercontent.com/apearsall20/apearsall20.github.io/master/by_state.csv");
-	console.log(data);
+	data = await d3.csv("https://raw.githubusercontent.com/apearsall20/apearsall20.github.io/master/US.csv");
+	console.log(data[0]);
 	return data;
 }
